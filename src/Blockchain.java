7fce19367764eb456a61d79a1d521553bb488952
Brenden Cai <caibrenden@gmail.com>
2019-03-06 01:55:44 +1100

    Implemented basic checks for adding new transaction messages
    Signed-off-by: Brenden Cai <caibrenden@gmail.com>

--- src/Blockchain.java
+++ src/Blockchain.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Blockchain {
-    private Block head;
+    private Block head = null;
     private ArrayList<Transaction> pool;
     private int length;
 
@@ -22,7 +22,41 @@ public class Blockchain {
 
     // add a transaction
     public int addTransaction(String txString) {
-        // TODO: implement you code here.
+        Transaction t = new Transaction();
+        // Construct transaction object
+        String[] st = txString.split("|");
+        if(st[0].equals("tx") && st.length == 3) {
+            t.setSender(st[1]);
+            t.setContent(st[2]);
+        }
+
+        //Validate and add transaction
+        if(t.validateSender() && t.validateContent()) {
+            pool.add(t);
+
+            // Check if pool limit reached
+            if (pool.size() >= poolLimit) {
+                // Create and add new block
+                Block block = new Block();
+
+                if(head == null) {
+                    block.setPreviousBlock(null);
+                    block.setPreviousHash(new byte[32]);
+                } else {
+                    block.setPreviousBlock(head);
+                    block.setPreviousHash(head.calculateHash());
+                }
+                block.setTransactions(getPool());
+
+                this.setHead(block);
+                this.setLength(this.getLength() + 1);
+                return 2;
+            }
+        } else {
+            return 0;
+        }
+
+        return 1;
     }
 
     public String toString() {
