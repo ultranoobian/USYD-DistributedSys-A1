ee037c417f5f53b0f5cc7f06aa1d712a9609d7be
Brenden Cai <caibrenden@gmail.com>
2019-03-05 23:40:55 +1100

    Implemented transaction (sender and content) validator methods
    Signed-off-by: Brenden Cai <caibrenden@gmail.com>

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,6 +1,9 @@
+import java.util.regex.*;
+
 public class Transaction {
     private String sender;
     private String content;
+    private boolean valid = false;
 
     // getters and setters
     public void setSender(String sender) { this.sender = sender; }
@@ -12,5 +15,15 @@ public class Transaction {
         return String.format("|%s|%70s|\n", sender, content);
     }
 
+    public boolean validateContent(String content) {
+        return ((content.length() >= 70) && (content.indexOf('|') == -1));
+    }
+
+    public boolean validateSender(String sender) {
+        Pattern unikeyPattern = Pattern.compile("[a-z]{4}[0-9]{4}");
+        Matcher unikeyMatcher = unikeyPattern.matcher(sender);
+
+        return unikeyMatcher.matches();
+    }
     // implement helper functions here if you need any
 }
