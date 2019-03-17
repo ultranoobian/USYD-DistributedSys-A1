74d775f5b2adceb4d4568a0a9fc16b8ef74b89c5
Brenden Cai <caibrenden@gmail.com>
2019-03-18 02:54:37 +1100

    Re-adding method signature as specified by the assignment document

--- src/BlockchainServer.java
+++ src/BlockchainServer.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.regex.Pattern;
@@ -62,6 +59,15 @@ public class BlockchainServer {
         }
     }
 
+    public void serverHandler(InputStream clientInputStream, OutputStream clientOutputStream) {
+
+        BufferedReader inputReader = new BufferedReader(
+                new InputStreamReader(clientInputStream));
+        PrintWriter outWriter = new PrintWriter(clientOutputStream, true);
+
+        // TODO: implement your code here.
+
+    }
     private static void handleSocketAcceptation(Socket sck, Blockchain blockchain) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(sck.getInputStream()));
         PrintWriter printWriter = new PrintWriter(sck.getOutputStream());
