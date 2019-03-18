6ea74b04f5794db506ecbfe43460562d0ceeb11c
Brenden Cai <caibrenden@gmail.com>
2019-03-18 16:20:01 +1100

    Added handling code for client application

--- src/BlockchainClient.java
+++ src/BlockchainClient.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class BlockchainClient {
@@ -11,18 +12,54 @@ public class BlockchainClient {
         int portNumber = Integer.parseInt(args[1]);
         BlockchainClient bcc = new BlockchainClient();
 
-        // TODO: implement your code here.
+        Socket sck;
+        try {
+            sck = new Socket(serverName, portNumber);
+
+            bcc.clientHandler(sck.getInputStream(), sck.getOutputStream());
+            sck.close();
+        } catch (IOException ioe) {
+
+        } finally {
+
+        }
     }
 
-    public void clientHandler(InputStream serverInputStream, OutputStream serverOutputStream) {
+    public void clientHandler(InputStream serverInputStream, OutputStream serverOutputStream) throws IOException {
         BufferedReader inputReader = new BufferedReader(
                 new InputStreamReader(serverInputStream));
         PrintWriter outWriter = new PrintWriter(serverOutputStream, true);
 
         Scanner sc = new Scanner(System.in);
+        int newLineCount = 0;
+        char incomingChar;
+        String stdLine;
         while (sc.hasNextLine()) {
-            // TODO: implement your code here
+
+            stdLine = sc.nextLine();
+            outWriter.println(stdLine);
+            outWriter.flush();
+
+            if (stdLine.equals("cc")) {
+                break;
+            }
+
+            while ((incomingChar = ((char) inputReader.read())) != -1) {
+                if (incomingChar == '\n') {
+                    newLineCount++;
+                } else {
+                    newLineCount = 0;
+                }
+                System.out.print(incomingChar);
+
+                if (newLineCount == 2) break;
+            }
+
         }
+
+        sc.close();
+        outWriter.close();
+        inputReader.close();
     }
 
     // implement helper functions here if you need any.
